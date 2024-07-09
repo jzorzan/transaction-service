e5b8fcfc1e57e0bb76c84d6ad94a7a7d09d1b7e1
Jonatas <jonatas.zorzan@gmail.com>
2024-07-09 10:37:19 -0300

    setup initial project strucutre

---
+++ src/main/java/br/com/challenge/caju/transaction/authorization/service/transaction/authorization/service/TransactionAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.caju.transaction.authorization.service.transaction.authorization.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAuthorizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge/caju/transaction/authorization/service/transaction/authorization/service/TransactionAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.caju.transaction.authorization.service.transaction.authorization.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
