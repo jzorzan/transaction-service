722f9645081eb7c9513f0d322436b4e15deafe20
Jonatas <jonatas.zorzan@gmail.com>
2024-07-11 21:32:18 -0300

    added Pessimistic lock

--- src/main/java/br/com/challenge/caju/transaction/service/gateways/repositories/AccountRepository.java
+++ src/main/java/br/com/challenge/caju/transaction/service/gateways/repositories/AccountRepository.java
@@ -1,9 +1,19 @@
 package br.com.challenge.caju.transaction.service.gateways.repositories;
 
 import br.com.challenge.caju.transaction.service.gateways.entities.Account;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    Optional<Account> findById(String accountId);
 }
